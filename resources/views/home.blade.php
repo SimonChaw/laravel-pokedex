245141d44f393b9c85c41737c4da160ed0661920
Simon Chawla <chawblah@gmail.com>
2024-05-24 14:35:33 -0300

    Initial Project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <h3>Welcome Trainer!</h1>
+    <p>Click Pokemon to search for and read about your favourite Pokemon, or click login to manage your trainer and Pokemon team!</p>
+</x-layout>
